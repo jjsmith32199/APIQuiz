0d3c2b6aefe3b3a17f830dd096484d8e31732cb2
jjsmith32199 <113246711+jjsmith32199@users.noreply.github.com>
2022-12-16 17:59:52 -0500

    created starter code for html JS and Css

---
+++ Assets/js/script.js
@@ -0,0 +1,24 @@
+let questions = [ 
+    {prompt: "What is the extension of a CSS file?\nA. .html\n\
+    B. .css\nC. .js",
+    answer: "B."
+    },
+    {
+    prompt: "How do you identify an ID in a CSS file?\nA.(#)\n\
+    B. (.)\nC. (!)",
+    answer: "A."
+    },
+    {
+    prompt: "How do you add interactivity to a webpage?\nA. CSS\n\
+    B. Javascript\nC. Add an 'Interact' label to your html file ",
+    answer: "B."
+    },
+    {
+    prompt: "What symbol is used to signify the start of a Jquery?\nA. (%)\n\
+    B. (@)\nC. ($)",
+    answer: "C."
+    },
+    {
+    prompt: ""
+    }]
+let score = 0
